d8d8f276d9c768dd40233070c45963fb3d2558fe
whisper650516-lab <whisper650516@gmail.com>
2026-06-22 14:59:17 +0900

    kadai_007提出

---
+++ kadai_007/associative-array.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>kadai007</title>
+</head>
+<body>
+  <?php
+  $ingredients1 = ['name' => 'onion', 'price' => 200, 'weight' => 160];
+  print_r($ingredients1);
+  ?>
+</body>
+</html>
