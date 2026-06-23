7efbf0d25a6b0b985c038d80220d08f47b9f14b3
whisper650516-lab <whisper650516@gmail.com>
2026-06-23 15:07:48 +0900

    kadai_011提出

---
+++ kadai_011/foreach.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>kadai_011</title>
+</head>
+<body>
+  <p>
+    <?php
+    $item = ['名前' => '玉ねぎ', '値段' => 200, '産地' => '北海道'];
+    foreach ($item as $key => $value) {
+      echo "{$key}：{$value}<br>";
+    }
+    ?>
+  </p>
+</body>
+</html>
